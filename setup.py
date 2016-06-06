f487c4b5e770b0190a43aa028d90b5acb0edd9f9
fran-bravo <fran_ase@hotmail.com>
2016-06-06 15:02:41 -0300

    Modified setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='socketpy',
     packages=['socketpy'],
-    version='v0.0.3',
+    version='v0.0.4',
     description='First description for the module',
     author='Francisco Bravo',
     author_email='fran_ase@hotmail.com',
     license='MIT',
     url='https://github.com/fran-bravo/socketpy',
-    download_url='https://github.com/fran-bravo/socketpy/tarball/v0.0.2',
+    download_url='https://github.com/fran-bravo/socketpy/tarball/v0.0.4',
     keywords=['sockets', 'C'],
     classifiers=['Development Status :: 3 - Alpha', ],
     entry_points={
