24dbc393890e206e1cfde97fbef346657eef2602
fran-bravo <fran_ase@hotmail.com>
2016-06-06 16:00:50 -0300

    setup

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='socketpy',
     packages=['socketpy'],
-    version='v0.0.8',
+    version='v0.0.9',
     description='First description for the module',
     author='Francisco Bravo',
     author_email='fran_ase@hotmail.com',
     license='MIT',
     url='https://github.com/fran-bravo/socketpy',
-    download_url='https://github.com/fran-bravo/socketpy/tarball/v0.0.8',
+    download_url='https://github.com/fran-bravo/socketpy/tarball/v0.0.9',
     keywords=['sockets', 'C'],
     classifiers=['Development Status :: 3 - Alpha', ],
     entry_points={
