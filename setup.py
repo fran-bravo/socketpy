e7440c4526cc31719fed1f530df148b3c96d3501
fran-bravo <fran_ase@hotmail.com>
2016-06-06 15:38:41 -0300

    Creation of source files

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from setuptools import setup
-from distutils.core import Extension
 
 setup(
     name='socketpy',
     packages=['socketpy'],
-    ext_modules=Extension('headers', sources=['modelos.h', 'paquetes.c', 'paquetes.h']),
-    version='v0.0.6',
+    version='v0.0.7',
     description='First description for the module',
     author='Francisco Bravo',
     author_email='fran_ase@hotmail.com',
     license='MIT',
     url='https://github.com/fran-bravo/socketpy',
-    download_url='https://github.com/fran-bravo/socketpy/tarball/v0.0.6',
+    download_url='https://github.com/fran-bravo/socketpy/tarball/v0.0.7',
     keywords=['sockets', 'C'],
     classifiers=['Development Status :: 3 - Alpha', ],
     entry_points={
--- socketpy/configure.py
+++ socketpy/configure.py
@@ -1,17 +1,20 @@
 import sqlite3, os
 from socketpy.excpetions import FileError
-from socketpy.filing import Filer
+from socketpy.filing import Filer, FileLineWrapper
 
 
 class Configure:
 
     def __init__(self):
         self.working_directory = os.getcwd()
-        self._create_directory()
+        self._create_directory("\\database")
+        self._create_directory("\\headers")
         self.database = os.path.dirname(os.path.abspath(__file__)) + "\\database\\types.db"
+        self.headers = os.path.dirname(os.path.abspath(__file__)) + "\\headers\\"
         self.conn = sqlite3.connect(self.database)
         self.cursor = self.conn.cursor()
         self.create_db()
+        self.create_headers()
 
     def create_db(self):
         self.cursor.execute("""CREATE TABLE types (type_id INTEGER PRIMARY KEY,
@@ -21,6 +24,12 @@ class Configure:
         self.conn.commit()
         self.cursor.close()
         self.conn.close()
+
+    def create_headers(self):
+        self._create_models()
+        self._create_packagesh()
+        self._create_packagesc()
+        return
 
     def _load_basic_types(self):
         types = [("int", 1), ("uint8_t", 1),
@@ -33,7 +42,135 @@ class Configure:
         self.cursor.executemany("INSERT INTO types VALUES(NULL, ?,?)", types)
 
     @staticmethod
-    def _create_directory():
-        if not os.path.exists(os.path.dirname(os.path.abspath(__file__)) + "\\database"):
-            os.makedirs(os.path.dirname(os.path.abspath(__file__)) + "\\database")
+    def _create_directory(directory):
+        if not os.path.exists(os.path.dirname(os.path.abspath(__file__)) + directory):
+            os.makedirs(os.path.dirname(os.path.abspath(__file__)) + directory)
 
+    def _create_models(self):
+        fd = FileLineWrapper(open(self.headers + "modelos.h", "w+"))
+        fd.f.writelines("""#ifndef MODELOS_H_
+                            #define MODELOS_H_
+
+                            #include  <commons/collections/list.h>
+
+                            typedef struct {
+                                int length;
+                                char *data;
+                            } t_stream;
+
+                            // Header de stream
+                            typedef struct {
+                                uint8_t tipoEstructura;
+                                uint16_t length;
+                            } __attribute__ ((__packed__)) t_header;
+
+                            // Modelos
+
+
+
+                            #endif"""
+                        )
+        fd.close()
+        return
+
+    def _create_packagesh(self):
+        fd = FileLineWrapper(open(self.headers + "paquetes.h", "w+"))
+        fd.f.writelines("""#ifndef PAQUETES_H_
+                            #define PAQUETES_H_
+
+                            #include <stdlib.h>
+                            #include <string.h>
+                            #include <stdio.h>
+                            #include <stdint.h>
+                            #include "string.h"
+                            #include "modelos.h"
+
+                            // Paquetizacion
+
+                            t_stream * paquetizar(int tipoEstructura, void * estructuraOrigen);
+
+                            // Despaquetizacion
+
+                            void * despaquetizar(uint8_t tipoEstructura, char * dataPaquete, uint16_t length);
+
+                            // Headers
+
+                            char * crearDataConHeader(uint8_t tipoEstructura, int length);
+                            t_header crearHeader(uint8_t tipoEstructura, uint16_t lengthDatos);
+
+                            t_header despaquetizarHeader(char * header);
+
+
+                            #endif"""
+                        )
+        fd.close()
+        return
+
+    def _create_packagesh(self):
+        fd = FileLineWrapper(open(self.headers + "paquetes.c", "w+"))
+        fd.f.writelines("""#include "paquetes.h"
+
+                            // Paquetizacion
+
+                            t_stream * paquetizar(int tipoEstructura, void * estructuraOrigen){
+                                t_stream * buffer;
+
+                                switch(tipoEstructura){
+                                } //Fin del switch
+
+                                return buffer;
+                            }
+
+                            // Despaquetizacion
+
+                            void * despaquetizar(uint8_t tipoEstructura, char * dataPaquete, uint16_t length){
+                                void * buffer;
+
+                                switch(tipoEstructura){
+                                } //Fin del switch
+
+                                return buffer;
+                            }
+
+                            // Headers
+
+
+                            char * crearDataConHeader(uint8_t tipoEstructura, int length){
+                                char * data = malloc(length);
+
+                                uint16_t lengthDatos = length - sizeof(t_header);
+
+                                t_header header = crearHeader(tipoEstructura, lengthDatos); //creo el header
+
+                                int tamanoTotal = 0, tamanoDato = 0;
+
+                                memcpy(data, &header.tipoEstructura, tamanoDato = sizeof(uint8_t)); //copio el tipoEstructura del header a data
+                                tamanoTotal = tamanoDato;
+                                memcpy(data + tamanoTotal, &header.length, tamanoDato = sizeof(uint16_t)); //copio el length del header a data
+
+                                return data;
+                            }
+
+                            t_header crearHeader(uint8_t tipoEstructura, uint16_t lengthDatos){
+                                t_header header;
+                                header.tipoEstructura = tipoEstructura;
+                                header.length = lengthDatos;
+                                return header;
+                            }
+
+                            t_header despaquetizarHeader(char * header){
+                                t_header estructuraHeader;
+
+                                int tamanoTotal = 0, tamanoDato = 0;
+                                memcpy(&estructuraHeader.tipoEstructura, header + tamanoTotal, tamanoDato = sizeof(uint8_t));
+                                tamanoTotal = tamanoDato;
+                                memcpy(&estructuraHeader.length, header + tamanoTotal, tamanoDato = sizeof(uint16_t));
+
+                                return estructuraHeader;
+                            }
+
+
+                            #endif"""
+                        )
+        fd.close()
+        return
